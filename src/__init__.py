b143280f216a890fcd6c62cfc0daba29f4dd6cb7
Spargelsalat <alexander.wolf1720@gmail.com>
2024-11-05 00:19:54 +0100

    transcriber

--- src/__init__.py
+++ src/__init__.py
@@ -4,3 +4,4 @@ from .srt_formatter import SRTFormatter
 from .file_handler import FileHandler
 
 __all__ = ["WhisperAudioTranscriber", "MarianTranslator", "SRTFormatter", "FileHandler"]
+DEFAULT_MODEL_SIZE = "medium"
--- src/transcriber.py
+++ src/transcriber.py
@@ -2,5 +2,14 @@ import whisper
 
 
 class WhisperAudioTranscriber:
-    def __init__(self):
-        pass
+    def __init__(self, model_size):
+        self.model = None
+        self.model_size = model_size
+
+    def load_model(self):
+        self.model = whisper.load_model(self.model_size, device="cpu")
+
+    def transcribe(self, file_path):
+        if not self.model:
+            self.load_model()
+        return self.model.transcribe(file_path, language="ja", verbose=True)
